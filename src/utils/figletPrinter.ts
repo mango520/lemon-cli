1020b74b9777d87cbfb614119fd91b6b43abebde
lemon <761264362@qq.com>
2025-07-26 23:21:46 +0800

    feat :0.0.6

--- src/utils/figletPrinter.ts
+++ src/utils/figletPrinter.ts
@@ -1,8 +1,8 @@
-const figlet = require ("figlet") ;
+// const figlet = require ("figlet") ;
 import chalk from 'chalk';
 
 export async function goodPrinter(){
   console.log(chalk.rgb(193, 190, 40).visible("欢迎使用lemon-front脚手架工具!"));
-  const data = await figlet('lemon-front');
-  console.log(chalk.rgb(193, 190, 40).visible(data));
+  // const data = await figlet('lemon-front');
+  // console.log(chalk.rgb(193, 190, 40).visible(data));
 };
